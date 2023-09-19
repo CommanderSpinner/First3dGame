1b146da0b24785df1156bd9ae55f3a356d6e2165
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2023-09-19 20:00:28 +0200

    Add files via upload

---
+++ ego shooter v0.5/game.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "raylib.h"
+#include <iostream>
+
+class Game {
+public:
+    Game();
+    ~Game();
+
+    void Init();
+    void Update();
+    void Draw();
+    void Shutdown();
+
+private:
+    Camera3D camera;
+    int cameraMode = CAMERA_FIRST_PERSON;
+
+    void drawTestCube(Vector3 cubePosition, float width, float height, float length);
+};
